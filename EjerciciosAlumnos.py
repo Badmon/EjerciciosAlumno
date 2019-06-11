4a6d4d307a08762e771a9de320c3a85ff6ac2a12
Badmon <jleonh@autonoma.edu.pe>
2019-06-11 03:41:12 -0500

    Commit

--- EjerciciosAlumnos.py
+++ EjerciciosAlumnos.py
@@ -1,3 +1,19 @@
-print ("HOLA")
-print ("HOLA")
-print ("HOLA")
+class Alumno:
+    def __init__(self,nombre,nota):
+        self.nombre = nombre
+        self.nota = nota
+    def imprimir(self):
+        print("Alumno: ",self.nombre)
+        print("Nota: ",self.nota)
+        print("")
+    
+    def resultado(self):
+        if self.nota<5:
+            print("El alumno ha reprobado")
+        else:
+            print("El alumno ha aprobado")
+
+alumno1=Alumno()
+
+alumno1.imprimir()
+alumno1.resultado()
